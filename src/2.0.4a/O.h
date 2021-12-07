9a267b2e97e49b802b90100bc706f84ca199a0b0
J Howard Johnson <j.howard.johnson@gmail.com>
2021-12-07 16:41:03 -0500

    Cleanup Alameq.c functions for C89

--- src/2.0.4a/O.h
+++ src/2.0.4a/O.h
@@ -181,7 +181,9 @@ A_OBJECT    A_lwds( char *, T_OBJECT );
 A_OBJECT    A_trim( A_OBJECT );
 
 // Alameq.c
-A_OBJECT        A_lameq();
+A_OBJECT    A_lameq( A_OBJECT );
+
+// Alamcm.c
 A_OBJECT        A_lamcm();
 A_OBJECT        A_clsure();
 A_OBJECT        A_subs();
