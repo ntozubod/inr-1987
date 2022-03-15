9e98f9b8ab970acaaa2d456b5bd2375c00c4a159
J Howard Johnson <j.howard.johnson@gmail.com>
2022-03-15 17:34:54 -0400

    Start of better UTF-8 for pr

--- work/src/T2.c
+++ work/src/T2.c
@@ -93,11 +93,61 @@ void T2_stats()
     Tn_stats();
 }
 
+int valid_utf8_at( char *s, int i, int l ) {
+    int c1 = s[ i ];
+    int type, c2, c3, c4;
+
+         if ( ( c1 & 0x80 ) == 0x00 ) { type = 0; }
+    else if ( ( c1 & 0xc0 ) == 0x80 ) { type = 1; }
+    else if ( ( c1 & 0xe0 ) == 0xc0 ) { type = 2; }
+    else if ( ( c1 & 0xf0 ) == 0xe0 ) { type = 3; }
+    else if ( ( c1 & 0xf8 ) == 0xf0 ) { type = 4; }
+    else { type = 5; }
+
+    switch ( type ) {
+
+    case 2:
+        if ( i + 1 >= l ) { return( 0 ); }
+        c2 = s[ i + 1 ];
+        if (    ( c2 & 0xc0 ) == 0x80 ) {
+            return( 2 );
+        }
+        break;
+
+    case 3:
+        if ( i + 2 >= l ) { return( 0 ); }
+        c2 = s[ i + 1 ];
+        c3 = s[ i + 2 ];
+        if (    ( c2 & 0xc0 ) == 0x80
+             && ( c3 & 0xc0 ) == 0x80 ) {
+            return( 3 );
+        }
+        break;
+
+    case 4:
+        if ( i + 3 >= l ) { return( 0 ); }
+        c2 = s[ i + 1 ];
+        c3 = s[ i + 2 ];
+        c4 = s[ i + 3 ];
+        if (    ( c2 & 0xc0 ) == 0x80
+             && ( c3 & 0xc0 ) == 0x80
+             && ( c4 & 0xc0 ) == 0x80 ) {
+            return( 4 );
+        }
+        break;
+
+    case 0: case 1: case 5:
+        break;
+    }
+    return( 0 );
+}
+
 void T2_sync( T2_OBJECT T2 )
 {
     char hexmap[17] = "0123456789ABCDEF";
     char *pr_str, *cstr;
     int i, j, k, length, next_ch, ii, pr_str_length;
+    int j2, utf8_len;
 
     Tn_OBJECT TT       = T2-> T2_int;
     Tn_OBJECT TT_print = T2-> T2_ext;
@@ -113,7 +163,14 @@ void T2_sync( T2_OBJECT T2 )
         k = 0;
         for( j = 0; j < length; ++j ) {
             next_ch = cstr[ j ];
-            if ( next_ch == ' ' ) {
+            utf8_len = valid_utf8_at( cstr, j, length );
+            if ( utf8_len > 0 ) {
+                pr_str[ k++ ] = next_ch;
+                for ( j2 = 1; j2 < utf8_len; ++j2 ) {
+                    next_ch = cstr[ ++j ];
+                    pr_str[ k++ ] = next_ch;
+                }
+            } else if ( next_ch == ' ' ) {
                 pr_str[ k++ ] = '\\';
                 pr_str[ k++ ] = '_';
             } else if ( next_ch == '\t' ) {
